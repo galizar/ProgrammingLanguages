83f3332655e48aa26e652e8089248fc6af7ca734
Luis Galicia <luisgalicia8@hotmail.com>
2019-12-15 22:19:41 -0400

    completed hw4 up to funny-number-stream

--- PartB/4/hw4.rkt
+++ PartB/4/hw4.rkt
@@ -5,3 +5,40 @@
 
 ;; put your code below
 
+(define (sequence low high stride)
+  (letrec ([fn-for-int (λ (curr acc)
+                         (if (> curr high)
+                             (reverse acc)
+                             (fn-for-int (+ curr stride) (cons curr acc))))])
+    (fn-for-int low empty)))
+
+(define (string-append-map xs suffix)
+  (map (λ (s) (string-append s suffix)) xs))
+
+(define (list-nth-mod xs n)
+  (if (> 0 n)
+      (error "list-nth-mod: negative number")
+      (letrec ([fn-for-lst (λ (lst curr-idx)
+                             (cond [(empty? lst) (error "list-nth-mod: empty list")]
+                                   [else
+                                    (let ([objective-idx (modulo n (length xs))])
+                                      (if (= curr-idx objective-idx)
+                                          (first lst)
+                                          (fn-for-lst (rest lst) (add1 curr-idx))))]))])
+        (fn-for-lst xs 0))))
+
+(define (stream-for-n-steps s n)
+  (letrec ([fn-for-stream (λ (stream acc steps-taken)
+                            (let ([next (stream)])
+                              (if (= n steps-taken)
+                                  (reverse acc)
+                                  (fn-for-stream (cdr next) (cons (car next) acc) (add1 steps-taken)))))])
+    (fn-for-stream s empty 0)))
+
+(define (funny-number-stream)
+  (letrec ([f (λ (x)
+                (let ([next (λ () (f (add1 x)))])
+                  (if (= 0 (modulo x 5))
+                      (cons (- x) next)
+                      (cons     x next))))])
+    (f 1)))
--- PartB/4/hw4test.rkt
+++ PartB/4/hw4test.rkt
@@ -18,40 +18,48 @@
    "Sample tests for Assignment 4"
    
    ; sequence test
-   (check-equal? (sequence 0 5 1) (list 0 1 2 3 4 5) "Sequence test")
-
+   (check-equal? (sequence 0 5 1) (list 0 1 2 3 4 5) "Sequence test 1")
+   (check-equal? (sequence 0 3 2) (list 0 2) "seqtest 2")
+   (check-equal? (sequence 0 1 3) (list 0) "seqtest 3")
+   (check-equal? (sequence 0 6 2) (list 0 2 4 6) "seqtest4")
+   
    ; string-append-map test
+   (check-equal? (string-append-map empty "lel") empty "sam test 1")
    (check-equal? (string-append-map 
                   (list "dan" "dog" "curry" "dog2") 
                   ".jpg") '("dan.jpg" "dog.jpg" "curry.jpg" "dog2.jpg") "string-append-map test")
    
    ; list-nth-mod test
    (check-equal? (list-nth-mod (list 0 1 2 3 4) 2) 2 "list-nth-mod test")
+   (check-equal? (list-nth-mod (list "h" "e" "l" "l" "o") 0) "h" "list-nth-mod test 1")
+   (check-equal? (list-nth-mod (list 0 1 2) 3) 0 "list-nth-mod test 2")
+   (check-equal? (list-nth-mod (list 0 1 2) 5) 2 "list-nth-mod test 3")
    
    ; stream-for-n-steps test
+   (check-equal? (stream-for-n-steps ones 0) empty "stream-for-n-steps test 0")
    (check-equal? (stream-for-n-steps ones 2) (list 1 1) "stream-for-n-steps test")
    
    ; funny-number-stream test
    (check-equal? (stream-for-n-steps funny-number-stream 16) (list 1 2 3 4 -5 6 7 8 9 -10 11 12 13 14 -15 16) "funny-number-stream test")
    
-   ; dan-then-dog test
-   (check-equal? (stream-for-n-steps dan-then-dog 1) (list "dan.jpg") "dan-then-dog test")
-   
-   ; stream-add-zero test
-   (check-equal? (stream-for-n-steps (stream-add-zero ones) 1) (list (cons 0 1)) "stream-add-zero test")
-   
-   ; cycle-lists test
-   (check-equal? (stream-for-n-steps (cycle-lists (list 1 2 3) (list "a" "b")) 3) (list (cons 1 "a") (cons 2 "b") (cons 3 "a")) 
-                 "cycle-lists test")
-   
-   ; vector-assoc test
-   (check-equal? (vector-assoc 4 (vector (cons 2 1) (cons 3 1) (cons 4 1) (cons 5 1))) (cons 4 1) "vector-assoc test")
-   
-   ; cached-assoc tests
-   (check-equal? ((cached-assoc (list (cons 1 2) (cons 3 4)) 3) 3) (cons 3 4) "cached-assoc test")
-   
-   ; while-less test
-   (check-equal? (while-less 7 do (begin (set! a (+ a 1)) a)) #t "while-less test")
+;   ; dan-then-dog test
+;   (check-equal? (stream-for-n-steps dan-then-dog 1) (list "dan.jpg") "dan-then-dog test")
+;   
+;   ; stream-add-zero test
+;   (check-equal? (stream-for-n-steps (stream-add-zero ones) 1) (list (cons 0 1)) "stream-add-zero test")
+;   
+;   ; cycle-lists test
+;   (check-equal? (stream-for-n-steps (cycle-lists (list 1 2 3) (list "a" "b")) 3) (list (cons 1 "a") (cons 2 "b") (cons 3 "a")) 
+;                 "cycle-lists test")
+;   
+;   ; vector-assoc test
+;   (check-equal? (vector-assoc 4 (vector (cons 2 1) (cons 3 1) (cons 4 1) (cons 5 1))) (cons 4 1) "vector-assoc test")
+;   
+;   ; cached-assoc tests
+;   (check-equal? ((cached-assoc (list (cons 1 2) (cons 3 4)) 3) 3) (cons 3 4) "cached-assoc test")
+;   
+;   ; while-less test
+;   (check-equal? (while-less 7 do (begin (set! a (+ a 1)) a)) #t "while-less test")
    
    ))
 
